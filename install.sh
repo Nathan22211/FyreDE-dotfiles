ef7d8e9fbd727c6fd72df79af4888ac56d639339
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-10-26 07:19:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #install
 sudo pacman -Sy
-sudo pacman -S nix swaync labwc pipwire pipewire-pulse wireplumber playerctl brightnessctl wlr-randr
+sudo pacman -S nix swaync labwc pipwire pipewire-pulse wireplumber playerctl brightnessctl wlr-randr hostapd dnsmasq
 
 sudo cp -r ./nix-modules /opt/
 sudo cp -r ./system /opt/
