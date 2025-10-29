0b61cce5bfb7e4fdc4c9227d920abcb9e0e9021e
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-10-28 21:23:54 -0500

    Delete nix-modules/waybar directory

--- nix-modules/waybar/data/waybar/scripts/bluetooth_status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/.nix-profile/bin:/run/current-system/sw/bin:/nix/var/nix/profiles/default/bin:$PATH"
-
-status=$(bluetoothctl show | grep "Powered:" | awk '{print $2}')
-if [ "$status" == "yes" ]; then
-    echo ""
-else
-    echo ""
-fi
