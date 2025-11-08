40fcb8c287a7136df180cea3812360fb4c27e798
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-11-07 22:03:02 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #install
 sudo pacman -Sy
-sudo pacman -S nix swaync labwc pipwire pipewire-pulse wireplumber playerctl brightnessctl wlr-randr hostapd dnsmasq
+sudo pacman -S nix swaync labwc pipwire pipewire-pulse wireplumber playerctl brightnessctl wlr-randr hostapd dnsmasq waybar wofi
 
 sudo cp -r ./nix-modules /opt/
 sudo cp -r ./system /opt/
 sudo cp -r ./labwc /ect/xdg/
-
-sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs
-sudo nix-channel --update -vvvvv
-
-sudo gpasswd -a $USER nixbld
