69b5bbeb2e5caf074554939e78b592ed663a0b82
Nathan22211 <35241068+Nathan22211@users.noreply.github.com>
2025-11-07 22:03:29 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 sudo pacman -Sy
 sudo pacman -S nix swaync labwc pipwire pipewire-pulse wireplumber playerctl brightnessctl wlr-randr hostapd dnsmasq waybar wofi
 
-sudo cp -r ./nix-modules /opt/
 sudo cp -r ./system /opt/
 sudo cp -r ./labwc /ect/xdg/
